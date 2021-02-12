ec615fd58e4de981adb1df4dd4afe6e0fa4805fd
Aleksandr <alextsishuk@gmail.com>
2021-02-13 01:23:24 +0300

    Lesson 53. Add carousel with JQuery script

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
-alert(1);
-[0,3].push;
-
-
-function abc(a,b){
-    console.log(a+b);
-}
-
-abc(10, 24);
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 400,
+		//adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
+});
