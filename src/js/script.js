291d73ad2ea7b2803e22e098620451bd95fd1b04
Aleksandr <alextsishuk@gmail.com>
2021-02-18 16:57:11 +0300

    Lesson 59. Add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 400,
-		//adaptiveHeight: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
@@ -33,4 +32,21 @@ $(document).ready(function () {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+	//Modal
+
+	$('[data-modal=consultation]').on('click', function(){
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+
+	$('.modal__close').on('click', function(){
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
 });
