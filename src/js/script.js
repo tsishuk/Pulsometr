55657d8741f4c3d8ce4d16e324f542e8aa12d38b
Aleksandr <alextsishuk@gmail.com>
2021-02-20 00:51:50 +0300

    Lesson 63. 12 minutes

--- src/js/script.js
+++ src/js/script.js
@@ -86,4 +86,19 @@ $(document).ready(function () {
     // Mask for telephone
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    // Sent mails
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
